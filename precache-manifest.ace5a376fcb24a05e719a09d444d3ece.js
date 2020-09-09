6e291a94a9473ecb44cb510922290c9198e541d7
Yuta Nakayama <nkymut@gmail.com>
2020-09-10 00:40:54 +0800

    Updates

---
+++ precache-manifest.ace5a376fcb24a05e719a09d444d3ece.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb28dc1311cc3acac44aba1a606e7a7e",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "a49a50718fd178597f10",
+    "url": "/reacttest/static/css/main.6fb05bca.chunk.css"
+  },
+  {
+    "revision": "f1101d73e0c694982c9e",
+    "url": "/reacttest/static/js/2.d48e1ed1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttest/static/js/2.d48e1ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a49a50718fd178597f10",
+    "url": "/reacttest/static/js/main.63eadbe3.chunk.js"
+  },
+  {
+    "revision": "7b65aa31a6ca0391dad8",
+    "url": "/reacttest/static/js/runtime-main.82a5519d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacttest/static/media/logo.5d5d9eef.svg"
+  }
+]);
